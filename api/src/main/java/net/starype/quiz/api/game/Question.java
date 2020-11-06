a33f0581d2d110b5a98d5090705a655acefd58ad
mredstone6 <mredstone6@gmail.com>
2020-11-06 19:42:06 +0100

    Update api/src/main/java/net/starype/quiz/api/game/Question.java
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>

--- api/src/main/java/net/starype/quiz/api/game/Question.java
+++ api/src/main/java/net/starype/quiz/api/game/Question.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface Question extends UUIDHolder {
 
-    List<QuestionTag> getTags();
+    Set<QuestionTag> getTags();
 
     default boolean isTagAttached(QuestionTag tag) {
         return getTags().contains(tag);
