50d401841abe5b7d82541772ab120c91c1f4a47e
mredstone6 <mredstone6@gmail.com>
2020-11-06 19:50:11 +0100

    feat: add more detail to the `Question` interface (#9)
    * feat: Question interface implementation
    
    * Update api/src/main/java/net/starype/quiz/api/game/Question.java
    
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>
    
    * Update api/src/main/java/net/starype/quiz/api/game/Question.java
    
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>
    
    * Update Question.java
    
    * Update api/src/main/java/net/starype/quiz/api/game/Question.java
    
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>
    
    * Update api/src/main/java/net/starype/quiz/api/game/Question.java
    
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>
    
    * Update Question.java
    
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>

---
+++ api/src/main/java/net/starype/quiz/api/game/Question.java
@@ -0,0 +1,25 @@
+package net.starype.quiz.api.game;
+
+import net.starype.quiz.api.game.player.UUIDHolder;
+
+import java.util.Set;
+
+public interface Question extends UUIDHolder {
+
+    Set<QuestionTag> getTags();
+
+    default boolean isTagAttached(QuestionTag tag) {
+        return getTags().contains(tag);
+    }
+
+    void registerTag(QuestionTag tag);
+
+    void unregisterTag(QuestionTag tag);
+
+    QuestionDifficulty getDifficulty();
+
+    String getRawQuestion();
+
+    String getDisplayableCorrectAnswer();
+
+}
