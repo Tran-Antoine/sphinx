112ca07d97e3d2d7c4d7c99396684a220d074aef
Guillaume Boyé <guillaume.boye@epfl.ch>
2020-11-18 12:09:10 +0100

    feat: add binary loss function (#19)
    Co-authored-by: mredstone6 <mredstone6@gmail.com>

---
+++ api/src/main/java/net/starype/quiz/api/game/answer/BinaryLossFunction.java
@@ -0,0 +1,19 @@
+package net.starype.quiz.api.game.answer;
+
+public class BinaryLossFunction implements LossFunction {
+    private double threshold = .5;
+
+    public BinaryLossFunction(double threshold) {
+        this.threshold = threshold;
+    }
+
+    public BinaryLossFunction() {
+        this(0.001);
+    }
+
+    @Override
+    public double evaluate(double x) {
+        return (x <= threshold) ? (1.0) : (0.0);
+    }
+
+}
