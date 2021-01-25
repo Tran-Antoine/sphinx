d9051e5286b42bae7180a4dc2b430b7b8c14a03c
Guillaume Boyé <guillaume.boye@epfl.ch>
2021-01-25 23:36:02 +0100

    Update api/src/main/java/net/starype/quiz/api/parser/ArgumentValue.java
    Co-authored-by: Antoine Tran <31257370+Tran-Antoine@users.noreply.github.com>

--- api/src/main/java/net/starype/quiz/api/parser/ArgumentValue.java
+++ api/src/main/java/net/starype/quiz/api/parser/ArgumentValue.java
@@ -3,7 +3,7 @@ package net.starype.quiz.api.parser;
 import java.util.Collection;
 
 /**
- * Class {@link ArgumentValue} defines an argument based on a name and a value
+ * Defines an argument based on a name and a value
  * @param <T> The type of the value associate to the name
  */
 public class ArgumentValue<T> implements Argument {
