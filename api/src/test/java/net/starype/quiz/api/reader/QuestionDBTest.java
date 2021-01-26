e31d5650a37588bc37b0daebbbd3b5c7b7d8705a
Guillaume Boyé <guillaume.boye@epfl.ch>
2021-01-26 15:51:52 +0100

    improvement: add abstraction and unit tests (#31)
    * improvement: retarget the QuestionDatabase to be an interface
    
    * improvement: improve the database system
    
    * change: adding file change
    
    * change: apply requests changes
    
    * change: apply requests change
    
    Co-authored-by: mredstone6 <mredstone6@gmail.com>

--- api/src/test/java/net/starype/quiz/api/reader/QuestionDBTest.java
+++ api/src/test/java/net/starype/quiz/api/reader/QuestionDBTest.java
@@ -1,16 +1,16 @@
 package net.starype.quiz.api.reader;
 
 import net.starype.quiz.api.game.question.Question;
-import net.starype.quiz.api.parser.QuestionDatabase;
-import net.starype.quiz.api.util.StringUtils;
+import net.starype.quiz.api.parser.SimpleQuestionDatabase;
+import net.starype.quiz.api.parser.QuestionQueries;
 
 import java.util.List;
 
 public class QuestionDBTest {
     public static void main(String[] args) {
-        QuestionDatabase db = new QuestionDatabase("api/src/main/resources/questions", "db.bin",
+        SimpleQuestionDatabase db = new SimpleQuestionDatabase("api/src/main/resources/questions", "db.bin",
                 false, false);
         db.sync();
-        List<Question> lQ = db.queryAll((data) -> data.getTags().contains("Math"));
+        List<Question> lQ = db.listQuery(QuestionQueries.allWithTag("Math"));
     }
 }
