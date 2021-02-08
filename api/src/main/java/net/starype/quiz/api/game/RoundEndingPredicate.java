264c7aaf82253c2375d8a78e16ef949b12728244
Michael ZHANG <57410827+firestar24@users.noreply.github.com>
2021-02-08 15:42:30 +0100

    Cleanup code

--- api/src/main/java/net/starype/quiz/api/game/RoundEndingPredicate.java
+++ api/src/main/java/net/starype/quiz/api/game/RoundEndingPredicate.java
@@ -9,32 +9,30 @@ public interface RoundEndingPredicate {
     void initRoundState(RoundState roundState);
 
     default RoundEndingPredicate or(RoundEndingPredicate other) {
-        RoundEndingPredicate first = this;
         return new RoundEndingPredicate() {
             @Override
             public boolean ends() {
-                return first.ends() || other.ends();
+                return RoundEndingPredicate.this.ends() || other.ends();
             }
 
             @Override
             public void initRoundState(RoundState roundState) {
-                first.initRoundState(roundState);
+                RoundEndingPredicate.this.initRoundState(roundState);
                 other.initRoundState(roundState);
             }
         };
     }
 
     default RoundEndingPredicate and(RoundEndingPredicate other) {
-        RoundEndingPredicate first = this;
         return new RoundEndingPredicate() {
             @Override
             public boolean ends() {
-                return first.ends() && other.ends();
+                return RoundEndingPredicate.this.ends() && other.ends();
             }
 
             @Override
             public void initRoundState(RoundState roundState) {
-                first.initRoundState(roundState);
+                RoundEndingPredicate.this.initRoundState(roundState);
                 other.initRoundState(roundState);
             }
         };
