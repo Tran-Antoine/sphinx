c57b4c26a4f24efc50c6157e0fc08fb9fcbab998
Antoine Tran <antoine2001.tran@gmail.com>
2022-02-12 21:41:16 +0100

    add weekly command

---
+++ discord-impl/src/main/java/net/starype/quiz/discordimpl/game/DiscordQuizTimer.java
@@ -0,0 +1,27 @@
+package net.starype.quiz.discordimpl.game;
+
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.starype.quiz.api.game.QuizTimer;
+
+import java.util.concurrent.TimeUnit;
+
+public class DiscordQuizTimer extends QuizTimer {
+
+    private final TextChannel channel;
+    private boolean sent = false;
+
+    public DiscordQuizTimer(TimeUnit unit, long time, TextChannel channel) {
+        super(unit, time);
+        this.channel = channel;
+    }
+
+    @Override
+    public void update(long deltaMillis) {
+        double left = millisLeft() / 1000.0;
+        if(left <= 30 && !sent) {
+            channel.sendMessage("> :warning: 20 seconds left!").queue(null, null);
+            sent = true;
+        }
+        super.update(deltaMillis);
+    }
+}
